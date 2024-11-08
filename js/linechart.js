41b08aa781edc327ce29a083e5229899b7ed7a53
hummingbee57 <bhasinmaya01@gmail.com>
2024-11-08 18:49:58 -0500

    bar charts no work

--- js/linechart.js
+++ js/linechart.js
@@ -1,165 +1,218 @@
 /* Fetch CSV temp data and display via chart.js 
     All temp values are differences from the mean of 14 deg-C */
 
-    async function getData(filePath) {
-        const response = await fetch(filePath);  // .. moves up one folder level
-        const data = await response.text();
-        // console.log(data);
+
+async function getData(filePath) {
+    const response = await fetch(filePath);  // .. moves up one folder level
+    const data = await response.text();
+    // console.log(data);
+    
+    const trials = [];
+    const meanAbsErrorTrain = [];
+    const meanAbsErrorTest = [];
+    const r2ScoreTrain = [];
+    const r2ScoreTest = [];
+
+    // \n - new line character
+    // split('\n') separate the table into an array of separate rows
+    // slice(start, end) - return array starting at index start and ending at end-1
+    const table = data.split('\n').slice(1);
+    table.pop();
+
+    table.forEach(row => {
+        const columns = row.split(',');  // split row into columns using commas 
+        const trial = parseFloat(columns[0]); // assign trial value
+        trials.push(trial);
         
-        const trials = [];
-        const meanAbsErrorTrain = [];
-        const meanAbsErrorTest = [];
-        const r2ScoreTrain = [];
-        const r2ScoreTest = [];
-    
-        // \n - new line character
-        // split('\n') separate the table into an array of separate rows
-        // slice(start, end) - return array starting at index start and ending at end-1
-        const table = data.split('\n').slice(1);
-        // console.log(table);
-    
-        table.forEach(row => {
-            const columns = row.split(',');  // split row into columns using commas 
-            const trial = parseFloat(columns[0]); // assign trial value
-            trials.push(trial);
-            
-            const maeTrain = parseFloat(columns[1]); // mae = mean absolute error
-            meanAbsErrorTrain.push(maeTrain);
-    
-            const maeTest = parseFloat(columns[2]); // mae = mean absolute error
-            meanAbsErrorTest.push(maeTest);
-    
-            const r2Train = parseFloat(columns[3]); // mae = mean absolute error
-            r2ScoreTrain.push(r2Train);
+        const maeTrain = parseFloat(columns[1]); // mae = mean absolute error
+        meanAbsErrorTrain.push(maeTrain);
 
-            const r2Test = parseFloat(columns[4]); // mae = mean absolute error
-            r2ScoreTest.push(r2Test);
+        const maeTest = parseFloat(columns[2]); // mae = mean absolute error
+        meanAbsErrorTest.push(maeTest);
+
+        const r2Train = parseFloat(columns[3]); // mae = mean absolute error
+        r2ScoreTrain.push(r2Train);
+
+        const r2Test = parseFloat(columns[4]); // mae = mean absolute error
+        r2ScoreTest.push(r2Test);
+
+        // console.log(year, temp, nhtemp, shtemp);
+    })
     
-            // console.log(year, temp, nhtemp, shtemp);
-        })
-        return {trials, meanAbsErrorTrain, meanAbsErrorTest, r2ScoreTrain, r2ScoreTest}  // return multiple values as a object
+    return {trials, meanAbsErrorTrain, meanAbsErrorTest, r2ScoreTrain, r2ScoreTest}  // return multiple values as a object
+}
+
+async function getAverages(metric, train) {
+    const average = arr => {
+        console.log(arr);
+        const sum = arr.reduce((acc, c) => {return acc + c});
+        console.log(sum);
+        const avg = sum / arr.length;
+        console.log(avg);
+        return avg;
     }
-    
-    async function createChart(metric, train) {    // metric can be 'mae' or 'r2', train can be true or false
-        // Get formatted data for all three area predictors
-        const controlData = await getData('../data/control_area.csv'); // wait for getData to get formatted data
-        const mlpData = await getData('../data/mlp_area.csv');
-        const mlrData = await getData('../data/mlr_area.csv');
 
-        const chartId = `${metric}-${train ? 'train' : 'test'}`
-        console.log(chartId)
-        const lineChart = document.getElementById(chartId);
+    const controlData = await getData('../data/control_area.csv'); // wait for getData to get formatted data
+    const mlpData = await getData('../data/mlp_area.csv');
+    const mlrData = await getData('../data/mlr_area.csv');
 
-        // Declare String Values based on metric and train/test
-        const prefix = metric === 'mae' ? 'meanAbsError' : 'r2Score';
-        const suffix = train ? 'Train' : 'Test';
-        const property = prefix + suffix;
-        console.log(property)
-        const metricTitle = metric === 'mae' ? 'Mean Absolute Error in sq. km.' : 'R^2 Score';
-        const metricTitleWithoutUnit = metric === 'mae' ? 'Mean Absolute Error' : 'R^2 Score'
-        const withIng = train ? 'Training' : 'Testing';
+    const prefix = metric === 'mae' ? 'meanAbsError' : 'r2Score';
+    const suffix = train ? 'Train' : 'Test';
+    const property = prefix + suffix;
 
-        const myChart = new Chart(lineChart, {
-            type: 'line',
-            data: {
-                labels: controlData.trials,
-                datasets: [
-                    {
-                        label: `${metricTitle} of the Control Area Predictor`,
-                        data: controlData[property],
-                        fill: false,
-                        backgroundColor: 'rbga(255, 0, 132, 0.2)',
-                        borderColor: 'rgba(255, 0, 132, 1)',
-                        borderWidth: 1
-                    },
-                    {
-                        label: `${metricTitle} of the Multilayer Perceptron Area Predictor`,
-                        data: mlpData[property],
-                        fill: false,
-                        backgroundColor: 'rbga(0, 102, 255, 0.2)',
-                        borderColor: 'rgba(0, 102, 255, 1)',
-                        borderWidth: 1
-                    },
-                    {
-                        label: `${metricTitle} of the Mulitple Linear Regression Area Predictor`,
-                        data: mlrData[property],
-                        fill: false,
-                        backgroundColor: 'rbga(0, 153, 51, 0.2)',
-                        borderColor: 'rgba(0, 153, 51, 1)',
-                        borderWidth: 1
-                    }
-                ]
-            },
-            options: {
-                responsive: true, // resize chart based on screen size
-                maintainAspectRatio: false,
-                scales: {                            // display options for x and y axes
-                    x: {
-                        title: {
-                            display: true,
-                            text: 'Trial #',         // x-axis title
-                            font: {
-                                size: 14
-                            }
-                        },
-                        ticks: {
-                            callback: function(val, index) {
-                                return index % 5 === 4 ? this.getLabelForValue(val) : '';
-                            },
-                            font: {
-                                size: 14
-                            }
-                        },
-                        grid: {
-                            color: '#6c767e'
-                        }
-                    },
-                    y: {
-                        title: {
-                            display: true,
-                            text: metricTitle,         // y-axis title
-                            font: {
-                                size: 14
-                            }
-                        },
-                        ticks: {
-                            min: 0,
-                            major: {
-                                enabled: true
-                            },
-                            font: {
-                                size: 12
-                            }
-                        },
-                        grid: {
-                            color: '#6c767e'
-                        },
-                        type: (metric === 'mae' && !train) ? 'logarithmic' : 'linear'
-                    }
+    return {
+        control: average(controlData[property]),
+        mlp: average(mlpData[property]),
+        mlr: average(mlrData[property])
+    }    
+}
+
+async function createBarChart(metric, train) {
+    data = await getAverages(metric, train)
+
+    const chartId = `${metric}-${train ? 'train' : 'test'}`
+    console.log(chartId)
+    const barChart = document.getElementById(chartId);
+
+    const xLabels = ['Control', 'Multilayer Perceptron', 'Multiple Linear Regression']
+    const yValues = [data.control, data.mlp, data.mlr];
+    console.log(yValues)
+
+    const myChart = new Chart(barChart, {
+        type: 'bar',
+        data: {
+            labels: xLabels,
+            datasets: [{
+                data: yValues
+            }]
+        }          
+        /* ,
+        'rgb(244, 152, 123)', // orange
+                        'rgb(232, 94, 92)'   // red */
+    })    
+}
+
+async function createChart(metric, train) {    // metric can be 'mae' or 'r2', train can be true or false
+    // Get formatted data for all three area predictors
+    const controlData = await getData('../data/control_area.csv'); // wait for getData to get formatted data
+    const mlpData = await getData('../data/mlp_area.csv');
+    const mlrData = await getData('../data/mlr_area.csv');
+
+    const chartId = `${metric}-${train ? 'train' : 'test'}`
+    console.log(chartId)
+    const lineChart = document.getElementById(chartId);
+
+    // Declare String Values based on metric and train/test
+    const prefix = metric === 'mae' ? 'meanAbsError' : 'r2Score';
+    const suffix = train ? 'Train' : 'Test';
+    const property = prefix + suffix;
+    console.log(property)
+    const metricTitle = metric === 'mae' ? 'Mean Absolute Error in sq. km.' : 'R^2 Score';
+    const metricTitleWithoutUnit = metric === 'mae' ? 'Mean Absolute Error' : 'R^2 Score'
+    const withIng = train ? 'Training' : 'Testing';
+    console.log(controlData[property])
+
+    const myChart = new Chart(lineChart, {
+        type: 'line',
+        data: {
+            labels: controlData.trials,
+            datasets: [
+                {
+                    label: `${metricTitle} of the Control Area Predictor`,
+                    data: controlData[property],
+                    fill: false,
+                    backgroundColor: 'rbga(255, 0, 132, 0.2)',
+                    borderColor: 'rgba(255, 0, 132, 1)',
+                    borderWidth: 1
                 },
-                plugins: {  // Display options for title and legends
+                {
+                    label: `${metricTitle} of the Multilayer Perceptron Area Predictor`,
+                    data: mlpData[property],
+                    fill: false,
+                    backgroundColor: 'rbga(0, 102, 255, 0.2)',
+                    borderColor: 'rgba(0, 102, 255, 1)',
+                    borderWidth: 1
+                },
+                {
+                    label: `${metricTitle} of the Mulitple Linear Regression Area Predictor`,
+                    data: mlrData[property],
+                    fill: false,
+                    backgroundColor: 'rbga(0, 153, 51, 0.2)',
+                    borderColor: 'rgba(0, 153, 51, 1)',
+                    borderWidth: 1
+                }
+            ]
+        },
+        options: {
+            responsive: true, // resize chart based on screen size
+            maintainAspectRatio: false,
+            scales: {                            // display options for x and y axes
+                x: {
                     title: {
                         display: true,
-                        text: `${metricTitleWithoutUnit} for Area Predictors over ${withIng} Datasets`,
+                        text: 'Trial #',         // x-axis title
                         font: {
-                            size: 24
-                        },
-                        color: '#black',
-                        padding: {
-                            top: 10,
-                            bottom: 30
+                            size: 14
                         }
                     },
-                    legend: {
-                        align: 'start',
-                        position: 'bottom'
+                    ticks: {
+                        callback: function(val, index) {
+                            return index % 5 === 4 ? this.getLabelForValue(val) : '';
+                        },
+                        font: {
+                            size: 14
+                        }
+                    },
+                    grid: {
+                        color: '#6c767e'
+                    }
+                },
+                y: {
+                    type: (metric === 'mae' && !train) ? 'logarithmic' : 'linear',
+                    title: {
+                        display: true,
+                        text: metricTitle,         // y-axis title
+                        font: {
+                            size: 14
+                        }
+                    },
+                    ticks: {
+                        min: 0,
+                        major: {
+                            enabled: true
+                        },
+                        font: {
+                            size: 12
+                        }
+                    },
+                    grid: {
+                        color: '#6c767e'
                     }
                 }
+            },
+            plugins: {  // Display options for title and legends
+                title: {
+                    display: true,
+                    text: `${metricTitleWithoutUnit} for Area Predictors over ${withIng} Datasets`,
+                    font: {
+                        size: 24
+                    },
+                    color: '#black',
+                    padding: {
+                        top: 10,
+                        bottom: 30
+                    }
+                },
+                legend: {
+                    align: 'start',
+                    position: 'bottom'
+                }
             }
-        })
-    }
+        }
+    })
+}
     
-createChart('mae', true);
-createChart('mae', false);
-createChart('r2', true);
-createChart('r2', false);
+createBarChart('mae', true);
+createBarChart('mae', false);
+createBarChart('r2', true);
+createBarChart('r2', false);
